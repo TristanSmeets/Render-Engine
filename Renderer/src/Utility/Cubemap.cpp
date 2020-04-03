78474525991c8abbce320ec7ca2efd9df13f50f8
Tristan Smeets <45764889+TristanSmeets@users.noreply.github.com>
2020-04-03 12:20:58 +0200

    Moved Cubemap class and split functionality
    Moved cubemap class to utility folder. Split texture creation to it's own function.

--- Renderer/src/Utility/Cubemap.cpp
+++ Renderer/src/Utility/Cubemap.cpp
@@ -1,15 +1,25 @@
 #include "Rendererpch.h"
 #include "Cubemap.h"
 
-Cubemap::Cubemap(GLsizei width, GLsizei height, GLuint internalFormat, GLenum format, GLenum type) :
-	width(width), height(height)
+Cubemap::Cubemap()
 {
+}
+
+Cubemap::~Cubemap()
+{
+}
+
+void Cubemap::CreateTexture(GLsizei width, GLsizei height, GLuint internalFormat, GLenum format, GLenum type)
+{
+	this->width = width;
+	this->height = height;
+
 	glGenTextures(1, &id);
 	glBindTexture(GL_TEXTURE_CUBE_MAP, id);
 
 	for (unsigned int i = 0; i < 6; ++i)
 	{
-		glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, internalFormat, width,height, 0, format, type, nullptr);
+		glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, internalFormat, width, height, 0, format, type, nullptr);
 	}
 
 	SetTextureParameter(GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
@@ -17,10 +27,6 @@ Cubemap::Cubemap(GLsizei width, GLsizei height, GLuint internalFormat, GLenum fo
 	SetTextureParameter(GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
 	SetTextureParameter(GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	SetTextureParameter(GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-}
-
-Cubemap::~Cubemap()
-{
 }
 
 void Cubemap::SetTextureParameter(GLenum name, GLint value)
@@ -41,4 +47,9 @@ const GLsizei Cubemap::GetWidth() const
 const GLsizei Cubemap::GetHeight() const
 {
 	return height;
+}
+
+void Cubemap::Bind()
+{
+	glBindTexture(GL_TEXTURE_CUBE_MAP, id);
 }
--- Renderer/src/Utility/Cubemap.h
+++ Renderer/src/Utility/Cubemap.h
@@ -5,16 +5,18 @@
 class Cubemap
 {
 public:
-	Cubemap(GLsizei width, GLsizei height, GLuint internalFormat, GLenum format, GLenum type);
+	Cubemap();
 	~Cubemap();
 	
+	void CreateTexture(GLsizei width, GLsizei height, GLuint internalFormat, GLenum format, GLenum type);
 	void SetTextureParameter(GLenum name, GLint value);
 	GLuint GetID() const;
 	const GLsizei GetWidth() const;
 	const GLsizei GetHeight() const;
+	void Bind();
 
 private:
 	GLuint id = 0;
-	GLsizei width;
-	GLsizei height;
+	GLsizei width = 0;
+	GLsizei height = 0;
 };
