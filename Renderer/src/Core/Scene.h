95713f6d9f9b1fe3bb62c8edf05d0b5501c5ec83
Tristan Smeets <45764889+TristanSmeets@users.noreply.github.com>
2020-04-05 16:32:41 +0200

    Created Scene class
    Created scene class and implemented functions that need to be implemented.

---
+++ Renderer/src/Core/Scene.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "Core/Actor.h"
+#include "Core/Light.h"
+#include "Core/Camera.h"
+
+#include "Rendering/Mesh.h"
+#include "Rendering/Material.h"
+#include "Rendering/Skybox.h"
+
+class Scene
+{
+public:
+	Scene();
+	virtual ~Scene();
+	const std::vector<Actor>& GetActors() const;
+	const std::vector<Light>& GetLights() const;
+	const Camera& GetCamera() const;
+	const Skybox& GetSkybox() const;
+	virtual void Initialize();
+
+protected:
+	virtual void InitializeMeshes() = 0;
+	virtual void InitializeMaterials() = 0;
+	virtual void InitializeActors() = 0;
+
+	std::vector<Actor> actors;
+	std::vector<Light> lights;
+	std::vector<Material> materials;
+	std::vector<Mesh> meshes;
+	Camera camera;
+	Skybox skybox;
+};
