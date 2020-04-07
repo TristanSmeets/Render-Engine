86d5f495a2899ab9a6f1de3abdf09a394fcc226d
Tristan Smeets <45764889+TristanSmeets@users.noreply.github.com>
2020-04-07 14:55:03 +0200

    Fixed NDCQuad not rendering correctly
    Didn't correctly set the mesh data and didn't call SetupMesh().

--- Renderer/src/Utility/NDCQuad.cpp
+++ Renderer/src/Utility/NDCQuad.cpp
@@ -13,25 +13,24 @@ NDCQuad::~NDCQuad()
 
 void NDCQuad::Initialize()
 {
-	Data quadData;
-	quadData.Vertices =
+	data.Vertices =
 	{
 		glm::vec3(-1.0f,  1.0f, 0.0f),
 		glm::vec3(-1.0f, -1.0f, 0.0f),
 		glm::vec3(1.0f,  1.0f, 0.0f),
 		glm::vec3(1.0f, -1.0f, 0.0f)
 	};
-	quadData.UVs =
+	data.UVs =
 	{
 		glm::vec2(0.0f, 1.0f),
 		glm::vec2(0.0f, 0.0f),
 		glm::vec2(1.0f, 1.0f),
 		glm::vec2(1.0f, 0.0f)
 	};
-	quadData.Indices =
+	data.Indices =
 	{
 		0,1,3,
 		1,2,3,
 	};
-	Mesh("NDCQuad", quadData);
+	SetupMesh();
 }
