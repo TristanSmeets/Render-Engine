2a9a1afcd62e47d1d6be3af0055cd21bdc642516
Tristan Smeets <45764889+TristanSmeets@users.noreply.github.com>
2020-04-10 17:26:39 +0200

    Added helper functions
    Added helper functions for Text, Transform, Colour, Vec3, Int

--- Renderer/src/Utility/GUIHelper.cpp
+++ Renderer/src/Utility/GUIHelper.cpp
@@ -3,6 +3,7 @@
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
+#include "imgui_stdlib.h"
 
 GUIHelper::GUIHelper()
 {
@@ -39,7 +40,78 @@ void GUIHelper::Render()
 
 	ImGui::ShowDemoWindow();
 
-	//ImGui::End();
+	ImGui::Begin("Scene");
+	RenderLayout();
+	ImGui::End();
+
 	ImGui::Render();
 	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
+
+void GUIHelper::RenderLayout()
+{
+	ImGui::Columns(2);
+
+	RenderText("Framerate");
+	ImGui::NextColumn();
+	RenderFPS();
+	ImGui::NextColumn();
+	ImGui::Separator();
+	RenderText("Name");
+	ImGui::NextColumn();
+	RenderText("Tristan");
+	ImGui::NextColumn();
+	ImGui::Separator();
+	RenderText("Transform");
+	ImGui::NextColumn();
+	if (ImGui::TreeNode(""))
+	{
+		RenderTransform(testTransform);
+		ImGui::TreePop();
+	}
+	ImGui::NextColumn();
+	ImGui::Separator();
+	RenderText("Colour");
+	ImGui::NextColumn();
+	RenderColour(light.GetColour());
+	ImGui::NextColumn();
+	RenderText("Int: ");
+	ImGui::NextColumn();
+	RenderInt("Width", test, 0, 1920);
+	
+}
+
+void GUIHelper::RenderFPS()
+{
+	RenderText("%.1f FPS (%.2f ms/frame) ", ImGui::GetIO().Framerate, 1000.0f / ImGui::GetIO().Framerate);
+}
+
+void GUIHelper::RenderText(const char* text, ...)
+{
+	va_list args;
+	va_start(args, text);
+	ImGui::Text(text, args);
+	va_end(args);
+}
+
+void GUIHelper::RenderTransform(const Transform & transform)
+{
+	RenderVec3("Position", transform.GetPosition());
+	RenderVec3("Rotation", transform.GetRotation());
+	RenderVec3("Scale", transform.GetScale());
+}
+
+void GUIHelper::RenderColour(const glm::vec3& colour)
+{
+	ImGui::ColorEdit3("Colour", (float*)&colour, ImGuiColorEditFlags_HDR | ImGuiColorEditFlags_Float);
+}
+
+void GUIHelper::RenderVec3(const char * name, const glm::vec3 & vec3)
+{
+	ImGui::DragFloat3(name, (float*)&vec3, 0.1f,-1000.0f,1000.0f,"%.2f");
+}
+
+void GUIHelper::RenderInt(const char * name, int & value, int minimum, int maximum)
+{
+	ImGui::DragInt(name, &value, 1.0f, minimum, maximum);
 }
--- Renderer/src/Utility/GUIHelper.h
+++ Renderer/src/Utility/GUIHelper.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "imgui.h"
+#include "Core/Light.h"
 #include "Core/Window.h"
+#include "Core/Transform.h"
 
 class GUIHelper
 {
@@ -9,4 +11,17 @@ public:
 	virtual ~GUIHelper();
 	void Initialize(const Window& window);
 	void Render();
+
+private:
+	void RenderLayout();
+	void RenderFPS();
+	void RenderText(const char* text, ...);
+	void RenderTransform(const Transform& transform);
+	void RenderColour(const glm::vec3& colour);
+	void RenderVec3(const char* name,const glm::vec3& vec3);
+	void RenderInt(const char* name, int& value, int minimum, int maximum);
+
+	Transform testTransform;
+	Light light = Light();
+	int test = 0;
 };
