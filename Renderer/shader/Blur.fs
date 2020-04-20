f6b6ff410e5dd8fc2e248e00428045b9e0fbb0f1
Tristan Smeets <45764889+TristanSmeets@users.noreply.github.com>
2020-04-20 12:32:49 +0200

    Added fragment shaders for bloom and blur
    Fragment shaders based on LearnOpenGL tutorials.

---
+++ Renderer/shader/Blur.fs
@@ -0,0 +1,48 @@
+#version 460 core
+out vec4 FragColour;
+
+in vec2 UV;
+
+uniform sampler2D image;
+
+subroutine vec3 directionalBlur(vec3 startValue, vec2 texOffset);
+subroutine uniform directionalBlur direction;
+
+float weight[5] = float[] (
+    0.2270270270, 0.1945945946, 0.1216216216, 
+    0.0540540541, 0.0162162162
+);
+
+subroutine(directionalBlur) vec3 Horizontal(vec3 startValue, vec2 texOffset)
+{
+    vec3 result = startValue;
+
+    for(int i = 0; i < 5; ++i)
+    {
+        result += texture(image, UV + vec2(texOffset.x * i, 0.0f)).rgb * weight[i];
+        result += texture(image, UV - vec2(texOffset.x * i, 0.0f)).rgb * weight[i];
+    }
+    return result;
+}
+
+subroutine(directionalBlur) vec3 Vertical(vec3 startValue, vec2 texOffset)
+{
+    vec3 result = startValue;
+
+    for(int i = 0; i < 5; ++i)
+    {
+        result += texture(image, UV + vec2(0.0f, texOffset.y * i)).rgb * weight[i];
+        result += texture(image, UV - vec2(0.0f, texOffset.y * i)).rgb * weight[i];
+    }
+    return result
+}
+
+void main()
+{
+    vec2 texOffset = 1.0f / textureSize(image, 0);
+    vec3 startValue = texture(image, UV).rgb * weight[0];
+
+    vec3 colour = directionalBlur(startValue, texOffset);
+
+    FragColour = vec4(colour, 1.0f);
+}
