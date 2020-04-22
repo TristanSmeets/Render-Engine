6bbe0225abb13b6a7d5834fd567265e59710a2ce
Tristan Smeets <45764889+TristanSmeets@users.noreply.github.com>
2020-04-22 16:18:40 +0200

    Created SSAO shaders
    Created shaders based on tutorial code.

---
+++ Renderer/shader/DeferredShading/SSAO.fs
@@ -0,0 +1,54 @@
+#version 460 core
+out float FragColour;
+
+in vec2 UV;
+
+uniform sampler2D gPosition;
+uniform sampler2D gNormal;
+uniform sampler2D noise;
+
+uniform vec3 samples[64];
+
+int kernelSize = 64;
+float radius = 0.5f;
+float bias = 0.025f;
+
+const vec2 noiseScale = vec2(1280.0f/4.0f, 720.0f/4.0f);
+
+uniform mat4 projection;
+
+void main()
+[
+    //Get input for SSAO algorithm
+    vec3 fragmentPosition = texture(gPosition, UV).xyz;
+    vec3 normal = normalize(texture(gNormal, UV).rgb);
+    vec3 randomVec = normalize(texture(noise, UV * noiseScale).xyz);
+    //Create TBN change of basis matrix: from tangent-space to view-space
+    vec3 tangent = normalize(randomVec - normal * dot(randomVec, normal));
+    vec3 bitangent = cross(normal, tangent);
+    mat3 TBN = mat3(tangent, bitangent, normal);
+    
+    //Iterate over the sample kernel and calculate occlusion factor
+    float occlusion = 0.0f;
+    for(int i = 0; i < kernelSize; ++i)
+    {
+        //Get sample position
+        vec3 sample = TBN * samples[i];
+        sample = fragmentPosition * sample * radius;
+
+        //Project sample position (to sample texture) (to get position on screen/texture)
+        vec4 offset = vec4(sample, 1.0f);
+        offset = projection * offset;
+        offset.xyz /= offset.w;
+        offset.xyz = offset.xyz * 0.5f + 0.5f;
+
+        //Get sample depth
+        float sampleDepth = texture(gPosition, offset.xy).z;
+
+        float rangeCheck = smoothstep(0.0f, 1.0f, radius / abs(fragmentPosition.z - sampleDepth));
+        occlusion += (sampleDepth >= sample.z + bias ? 1.0f : 0.0) * rangeCheck;
+    }
+    occlusion = 1.0f - (occlusion / kernelSize);
+
+    FragColour = occlusion;
+]
---
+++ Renderer/shader/DeferredShading/SSAOBlur.fs
@@ -0,0 +1,21 @@
+#version 460 core
+out float FragmentColour;
+
+in vec2 UV;
+
+uniform sampler2D ssaoInput;
+
+void main()
+{
+    vec2 texelSize = 1.0f/ vec2(textureSize(ssaoInput, 0));
+    float result = 0.0f;
+    for (int x = -2; x < 2; ++x)
+    {
+        for(int y = -2; y < 2; ++y)
+        {
+            vec2 offset = vec2(float(x), float(y)) * texelSize;
+            result += texture(ssaoInput, UV + offset).r;
+        }
+    }
+    FragmentColour = result / (4.0f * 4.0f);
+}
